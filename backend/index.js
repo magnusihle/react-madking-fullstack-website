6e8ed5d06b6332e0a10d10ff0a4f67e7ae74e835
Johannes Erdahl Andresen <johannes.erdahl.andresen@gmail.com>
2021-12-10 14:14:54 +0100

    make it better

--- backend/index.js
+++ backend/index.js
@@ -33,3 +33,12 @@ app.use("/api/products", productRoute);
 app.listen(process.env.PORT || 5000, () => {
     console.log("Server is running..");
 });
+
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
