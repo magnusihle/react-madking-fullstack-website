068d7ae1f84ba3fbc85718e9e60b0f57ce5019fd
Johannes Erdahl Andresen <johannes.erdahl.andresen@gmail.com>
2021-12-11 22:42:26 +0100

    Add build folder to backend

--- backend/index.js
+++ backend/index.js
@@ -37,9 +37,9 @@ app.listen(process.env.PORT || 5000, () => {
 
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-// All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
