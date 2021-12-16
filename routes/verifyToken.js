3f8218ce88fee240dab1dba67029c7b6bbdc8c20
Johannes Erdahl Andresen <johannes.erdahl.andresen@gmail.com>
2021-12-16 12:06:13 +0100

    test2

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -8,7 +8,7 @@ const verifyToken = (req, res, next) => {
         const token = authHeader.split(" ")[1];
         jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
             if (err) {
-                res.status(403).json("Token is not valid!");
+                res.status(403).json("Token is not valid.");
             } else {
                 req.user = user;
                 next();
