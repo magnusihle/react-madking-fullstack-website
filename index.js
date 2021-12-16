96eee1a59431c816e113913c9addfe7e84d396c6
Johannes Erdahl Andresen <johannes.erdahl.andresen@gmail.com>
2021-12-16 12:27:05 +0100

    Send mwssage when server is running

--- index.js
+++ index.js
@@ -35,11 +35,15 @@ app.use("/api/products", productRoute);
 
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.join(__dirname, "./client")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build', 'index.html'));
-});
+app.get("/", (req, res) => {
+    res.send("Server is running..")
+})
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, './client/build'));
+// });
 
 
 app.listen(process.env.PORT || 5000, () => {
