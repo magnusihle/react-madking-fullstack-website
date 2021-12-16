e1b016345fb8d19fcdf85be8c943891260565424
Magnus Ihle <magnus.ihle@sysco.no>
2021-12-16 14:08:55 +0100

    Setting up dev

---
+++ client/cypress/integration/init.spec.js
@@ -0,0 +1,10 @@
+describe("Cypress", () => {
+  it("is working", () => {
+    // eslint-disable-next-line jest/valid-expect
+    expect(true).to.equal(true);
+  });
+
+  it("visits the app", () => {
+    cy.visit("/");
+  });
+});
